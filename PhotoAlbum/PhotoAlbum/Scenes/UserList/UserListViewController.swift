81750348771f2ef67e8b02ff609efa8f14ee745d
Onur Torna <onur.torna@mobven.com>
2019-04-10 20:50:08 +0300

    Add table view to landing page

--- PhotoAlbum/PhotoAlbum/Scenes/UserList/UserListViewController.swift
+++ PhotoAlbum/PhotoAlbum/Scenes/UserList/UserListViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class UserListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    let viewModel = UserListViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.register(UserListCell.defaultNib,
+                           forCellReuseIdentifier: UserListCell.defaultNibName)
+
+        viewModel.stateChangeHandler = applyState(_:)
+        viewModel.fetchUsers()
     }
-
-
 }
 
+// MARK: - Helpers
+private extension UserListViewController {
+
+    func applyState(_ change: UserListState.Change) {
+
+        switch change {
+        case .loading(let isLoading):
+            // TODO: Show/hide loading view
+            break
+
+        case .error(let receivedError):
+            // TODO: Show/hide error view
+            break
+
+        case .dataFetch:
+            tableView.reloadData()
+        }
+    }
+}
+
+extension UserListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.userCount
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: UserListCell.defaultNibName) as? UserListCell else {
+            return UITableViewCell()
+        }
+
+        let user = viewModel.user(at: indexPath.row)
+        cell.userNameLabel.text = user.fullname
+        // TODO: customize cell
+        return cell
+    }
+}
